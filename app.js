7d02cb4dec1bf650e367c6f3c1b4b4054e3eb375
mikemiksch <mikemiksch@gmail.com>
2016-10-20 14:01:31 -0700

    Saving progress completed on app.js so far.

--- app.js
+++ app.js
@@ -1,85 +1,110 @@
 'use strict';
-var country = confirm('Have i visited more than 10 country?');
-if(country === true) {
-  alert('guess again');
-  console.log('I have visited only 7 country');
-} else {
-  alert('You Got it.');
-}
-var lived = prompt('Have I lived in US more than 20 yrs?');
-if(lived === 'yes') {
-  console.log(lived + ' is Correct');
-} else {
-  console.log('I lived here 27 yrs');
-  alert('Boo Boo!!!');
-  console.log('alphabet'.toUpperCase());
-  console.log('alphabet'.toLowerCase());
-}
-var kids = confirm('Do I have kids in High School?');
-if(kids === false) {
-  alert('still in middle School');
-} else {
-  alert('NOO NOO...');
-  console.log('One more year to go!!!');
-}
-var food = prompt('Have I eat frog legs before?');
-if(food === 'yes') {
-  console.log(food + ' ,taste like chicken');
-} else {
-  alert('You missing a good stuff..');
-  console.log('alphabet'.toUpperCase());
-  console.log('alphabet'.toLowerCase());
-}
-var music = confirm('Do I like to listen to Heavy Metal Music?');
-if(music === false) {
-  alert('Couldn\'t handle it');
-} else {
-  alert('Never, Never, Never');
-  console.log(music + ' ,I don\'t like to listen to Heavy Metal Music');
-}
-var time = 0;
-while(true) {
-  var number = prompt('Guess a number between 1 and 10?');
-  if(number === '7') {
-    alert(number + ' is the right number');
-    break;
-  } else if(time > 3) {
-    alert('You didn\'t guess it');
-    console.log('You didn\'t guess it');
-    break;
+
+function question1() {
+  var country = confirm('Have i visited more than 10 country?');
+  if(country === true) {
+    alert('guess again');
+    console.log('I have visited only 7 country');
+  } else {
+    alert('You Got it.');
   }
-  time++;
-  console.log('you guess ' + time);
 }
 
-var water = 0;
-while(true) {
-  var cup = prompt('How many cup of water should you drink in 1 day');
-  if(cup === '8') {
-    alert(cup + ' is correct');
-    break;
-  } else if(water > 3) {
-    alert('The correct anser is 8');
-    console.log('The correct anser is 8');
-    break;
+function question2() {
+  var lived = prompt('Have I lived in US more than 20 yrs?');
+  if(lived.toLowerCase() === 'yes' || lived.toLowerCase() === 'y') {
+    alert('That is Correct');
+    console.log('That is is Correct');
+  } else {
+    console.log('I lived here 27 yrs');
+    alert('I\'ve lived here 27 years');
   }
-  water++;
-  console.log(water);
 }
 
-while(true) {
-  var musicString = prompt('How many hours I listen to Music?');
-  var music = parseInt(musicString);
-  if(music === 5) {
-    alert(music + ' hours is correct');
-    break;
-  } else if(music < 10 && music > 5 ) {
-    alert('Too high');
-  } else if(music > 0) {
-    alert('Too low');
-    break;
+function question3() {
+  var kids = confirm('Do I have kids in High School?');
+  if(kids === false) {
+    alert('still in middle School');
+  } else {
+    alert('NOO NOO...');
+    console.log('One more year to go!!!');
   }
-  console.log('Better luck next time');
+}
+
+function question4() {
+  var food = prompt('Have I eat frog legs before?');
+  if(food.toLowerCase() === 'yes' || food.toLowerCase() === 'y') {
+    alert('Tastes like chicken');
+    console.log(food + ' ,taste like chicken');
+  } else {
+    alert('You missing a good stuff..');
+    console.log('You\'re missing good stuff.');
+  }
+}
+
+function question5() {
+  var music = confirm('Do I like to listen to Heavy Metal Music?');
+  if(music === false) {
+    alert('Couldn\'t handle it');
+    console.log('Couldn\'t handle it.');
+  } else {
+    alert('Never, Never, Never');
+    console.log('I don\'t like to listen to Heavy Metal Music');
+  }
+}
+
+// function question6() {
+//   var time = 0;
+//   while(true) {
+//     var number = prompt('Guess a number between 1 and 10?');
+//     if(number === '7') {
+//       alert('7 is the right number');
+//       break;
+//     } else if(time > 3) {
+//       alert('You didn\'t guess it');
+//       console.log('You didn\'t guess it');
+//       break;
+//     }
+//     time++;
+//     console.log('you guess ' + time);
+//   }
+//  var water = 0;
+// while(true) {
+//   var cup = prompt('How many cup of water should you drink in 1 day');
+//   if(cup === '8') {
+//     alert(cup + ' is correct');
+//     break;
+//   } else if(water > 3) {
+//     alert('The correct anser is 8');
+//     console.log('The correct anser is 8');
+//     break;
+//   }
+//   water++;
+//   console.log(water);
+// }
+
+function question6() {
+  var tries = 1;
+  while(true) {
+    var musicString = prompt('How many hours I listen to Music?');
+    var music = parseInt(musicString);
+    if(music === 5) {
+      alert('5 hours is correct');
+      console.log('5 hours is corret');
+      break;
+    } else if (music > 5 && tries < 4) {
+      alert('Too high');
+      console.log('Too high');
+    } else if (music < 5 && tries < 4) {
+      alert('Too low');
+      console.log('Too low');
+    } else {
+      alert('Better luck next time');
+      console.log('Better luck next time');
+      break;
+    }
+    tries += 1;
+  }
 }
 
 var characters = 0;
@@ -108,7 +133,13 @@ while(true) {
   }
   height++;
 }
-
+  var music = confirm('Do I like to listen to Heavy Metal Music?');
+  if(music === false) {
+    alert('Couldn\'t handle it');
+  } else {
+    alert('Never, Never, Never');
+    console.log(music + ' ,I don\'t like to listen to Heavy Metal Music');
+  }
 while(true) {
   var duiString = prompt('What is the alcohol level to be consider as DUI?');
   var number = parseInt(duiString);
